edaca4b6298dcb166f0e7c3b0e3ac8cd5a54b0e5
alexandra Brown <student@epicodus.com>
2015-08-21 13:54:38 -0700

    create route to .stylist page

--- app/app.php
+++ app/app.php
@@ -13,11 +13,25 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
 
-    use Symfony\Component\HttpFoundation\Request;
-    Request::enableHttpMethodParameterOverride();
+    // use Symfony\Component\HttpFoundation\Request;
+    // Request::enableHttpMethodParameterOverride();
 
+    //Landing page - displays any stylists that have been created
     $app->get("/", function() use($app) {
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    //Allows user to create new stylist, saves to the database and displays on homepage
+    $app->post("/stylists", function() use($app) {
+        $stylist = new Stylist($_POST['name']);
+        $stylist->save();
+        return $app['twig']->render('index.html.twig', array('stylists' =>Stylist::getAll()));
+    });
+
+    //Takes user to a specific stylist page
+    $app->get("/stylists/{id}", function($id) use($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylists.html.twig', array('stylists' => $stylist, 'clients' => $stylist->getClients()));
     });
 
 
